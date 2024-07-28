502142fd81674fc6a0b2b6b79cc7b2faaccdc26f
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-07-28 13:40:42 +0530

    Commit

---
+++ topological_sort.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int topo[10], k;
+
+void dfs(int a[10][10], int n, int visited[10],int source){
+    visited[source]=1;
+
+    for(int i=1; i<=n;i++){
+        if(!visited[i]&&a[source][i]==1)
+            dfs(a,n,visited,i);
+    }
+    topo[++k] = source;
+}
+
+int main(){
+    int n,a[10][10];
+    int visited[10]={0};
+
+    printf("\n Enter the no of vertices : ");
+    scanf("%d",&n);
+
+    printf("\n Enter the adjacency matrix\n");
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(int i=1;i<=n;i++){
+        if(!visited[i])
+            dfs(a,n,visited,i);
+    }
+
+    printf("\n The topological ordering is \n");
+    for(int i=k;i>=1;i--)
+        printf("%d\t",topo[i]);
+    
+    return 0;
+}
