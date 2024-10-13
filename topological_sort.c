5add2da6962652947998e8a92c79d32daee65e93
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-10-14 01:09:43 +0545

    Updated topological_sort program with cycle detection

--- topological_sort.c
+++ topological_sort.c
@@ -1,39 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
+int topo[10],k;
+int cycle_detected = 0;
 
-int topo[10], k;
-
-void dfs(int a[10][10], int n, int visited[10],int source){
-    visited[source]=1;
-
-    for(int i=1; i<=n;i++){
-        if(!visited[i]&&a[source][i]==1)
-            dfs(a,n,visited,i);
+void dfs(int a[][10], int n, int visited[], int rec_stack[], int source){
+  int i;
+  visited[source] = 1;
+  rec_stack[source] = 1;
+  for(i=1;i<=n;i++){
+    if(a[source][i]){
+      if(!visited[i]){
+        dfs(a,n,visited,rec_stack,i);
+      }
+      else if(rec_stack[i]){
+        cycle_detected = 1;
+      }
     }
-    topo[++k] = source;
+  }
+  rec_stack[source] = 0;
+  topo[++k] = source;
 }
 
 int main(){
-    int n,a[10][10];
-    int visited[10]={0};
-
-    printf("\n Enter the no of vertices : ");
-    scanf("%d",&n);
-
-    printf("\n Enter the adjacency matrix\n");
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            scanf("%d",&a[i][j]);
-        }
+  int i,j,n,a[10][10], visited[10]={}, rec_stack[10]={};
+  
+  printf("\n Enter the number of nodes in the graph : \n");
+  scanf("%d",&n);
+  
+  printf("\n Enter the adjacency matrix of the graph : \n");
+  for(i=1;i<=n;i++){
+    for(j=1;j<=n;j++){
+      scanf("%d",&a[i][j]);
     }
-    for(int i=1;i<=n;i++){
-        if(!visited[i])
-            dfs(a,n,visited,i);
+  }
+  
+  for(i=1;i<=n;i++){
+    if(!visited[i]){
+      dfs(a,n,visited,rec_stack,i);
     }
-
-    printf("\n The topological ordering is \n");
-    for(int i=k;i>=1;i--)
-        printf("%d\t",topo[i]);
-    
-    return 0;
+  }
+  
+  if(cycle_detected){
+    printf("\n Cycle detected in the graph\n");
+  }
+  else{
+    printf("\n Topological Ordering is : \n");
+    for(i=k;i>=1;i--)
+      printf("%d\t",topo[i]);
+    printf("\n");
+  }
+  return 0;
 }
