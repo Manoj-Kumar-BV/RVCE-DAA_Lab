64c1c2ab415f861b94bac6d2cc950b47db4b9030
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-10-14 20:05:28 +0545

    Updated knapsack.c with Dynamic Programming

--- knapsack.c
+++ knapsack.c
@@ -1,72 +1,69 @@
 #include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
 
-#define SIZE 256
-
-int finalvalues[SIZE][SIZE], weights[SIZE], values[SIZE];
-int value, capacity;
+int n,W,w[10],v[10],V[10][10],x[10] = {};
 
 int max(int a, int b){
-    return a>b?a:b;
+  return (a>b) ? a:b;
 }
 
-int MFK(int i, int j){
-    if(i==0 || j==0)
-        return 0;
-    
-    if(finalvalues[i][j]<0){
-        if(j<weights[i])
-            value = MFK(i-1,j);
-        else
-            value=max(MFK(i-1,j),values[i]+MFK(i-1,j-weights[i]));
-        
-        finalvalues[i][j] = value;
+void display(){
+  int i=n, j=W;
+  while(i>0 && j>0){
+    if(V[i][j] != V[i-1][j]){
+      x[i] = 1;
+      j = j-w[i];
     }
-    return finalvalues[i][j];
+    i=i-1;
+  }
 }
 
 int main(){
-    int n;
-    printf("Enter knapsack capacity: ");
-    scanf("%d",&capacity);
-
-    printf("Enter number of objects: ");
-    scanf("%d",&n);
-
-    printf("Enter profits of %d objects: ",n);
-    for(int i=1;i<=n;i++)
-        scanf("%d",&values[i]);
-    
-    printf("Enter the weights of %d objects: ",n);
-    for(int i=1;i<=n;i++)
-        scanf("%d",&weights[i]);
-    
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=capacity;j++)
-            finalvalues[i][j] = -1;
+  int i,j;
+  
+  printf("\n Enter the number of items : \n");
+  scanf("%d",&n);
+  
+  printf("\nEnter the weights of the items : \n");
+  for(i=1;i<=n;i++)
+    scanf("%d",&w[i]);
+  
+  printf("\n Enter the profits/values of the items : \n");
+  for(i=1;i<=n;i++)
+    scanf("%d",&v[i]);
+  
+  printf("\n Enter the maximum capacity of the knapsack : \n");
+  scanf("%d",&W);
+  
+  for(i=0;i<=n;i++){
+    for(j=0;j<=W;j++){
+      if(i==0 || j==0)
+        V[i][j] = 0;
+      else if(w[i]>j)
+        V[i][j] = V[i-1][j];
+      else
+        V[i][j] = max(V[i-1][j],V[i-1][j-w[i]]+v[i]);
     }
-
-    int finalprofit = MFK(n,capacity);
-    printf("\n Final profit is : %d\n",finalprofit);
-
-    printf("Selected objects are: ");
-    int i=n, j=capacity;
-
-    while(i != 0 && j != 0){
-        if(finalvalues[i][j] != finalvalues[i-1][j]){
-            printf("%d",i);
-            j=j-weights[i];
-        }
-        i--;
+  }
+  
+  display();
+  
+  printf("\n Maximum capacity of the knapsack is : %d\n",V[n][W]);
+  
+  printf("\n Items included are : \n");
+  for(i=1;i<=n;i++){
+    if(x[i]){
+      printf("\n Item %d : (Weight: %d, Profit: %d) is included \n",i,w[i],v[i]);
     }
-
-    printf("\n\n Table Values are : \n");
-    for(int i=0; i<=n; i++){
-        for(int j=0;j<=capacity;j++)
-            printf("%d\t",finalvalues[i][j]);
-        printf("\n");
+  }
+  
+  printf("\n Table Values are : \n");
+  for(i=0;i<=n;i++){
+    for(j=0;j<=W;j++){
+      printf("%d\t",V[i][j]);
     }
-
-    return 0;
+    printf("\n");
+  }
+  
+  return 0;
 }
