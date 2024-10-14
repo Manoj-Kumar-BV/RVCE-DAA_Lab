c8d819a07815b6e8741048695a79030c30646b39
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-10-14 20:06:03 +0545

    Updated easy horspool.c

--- horspool.c
+++ horspool.c
@@ -1,65 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#define SIZE 256
 
-#define SIZE 1000
+int Table[SIZE];
 
-int table[SIZE];
-int indices[SIZE];
-int j,i,shifts;
-
-void shiftTable(char p[]){
-    int m=strlen(p);
-    for(int i=0;i<SIZE;i++)
-        table[i] = m;
-    for(int j=0;j<m-1;j++)
-        table[p[j]] = m-1-j;    
+void shiftTable(char p[10]){
+  int m = strlen(p), i;
+  for(i=0;i<SIZE;i++)
+    Table[i] = m;
+  for(i=0;i<=m-2;i++)
+    Table[(int)p[i]]=m-1-i;
 }
 
-void horspool(char t[], char p[]){
-    int n=strlen(t);
-    int m=strlen(p);
-    i=m-1;
-    int k;
-
-    shiftTable(p);
-    while(i<n){
-        k=0;
-        while(k<m && p[m-1-k]==t[i-k])
-            k++;
-        if(k==m){
-            indices[j++]=i-m+1;
-            i=i+table[t[i]];
-            shifts++;
-        }
-        else{
-            i=i+table[t[i]];
-            shifts++;
-        }
-    }
+int horspool(char t[SIZE], char p[10]){
+  int m = strlen(p),n = strlen(t),i,k;
+  shiftTable(p);
+  i = m-1;
+  while(i<n){
+    k=0;
+    while(k < m && p[m-1-k] == t[i-k])
+      k++;
+    if(k==m)
+      return i-m+1;
+    else
+      i=i+Table[(int)t[i]];
+  }
+  return -1;
 }
 
 int main(){
-    char p[SIZE],t[SIZE];
-    printf("Enter text: ");
-    scanf("%[^\n]%*c",t);
-
-    printf("Enter pattern: ");
-    scanf("%[^\n]%*c",p);
-
-    j=0;
-    horspool(t,p);
-
-    if(j==0)
-        printf("Pattern not found");
-    else{
-        printf("Pattern found at indices: ");
-        for(int i=0;i<j;i++){
-            printf("%d",indices[i]);
-            if(i<j-1)
-                printf(", ");
-        }
-        printf("\n Number of shifts performed are: %d\n",shifts);
-    }
-    return 0;
+  char text[SIZE], pattern[SIZE];
+  
+  printf("\n Enter the text : \n");
+  scanf("%s",text);
+  
+  printf("\n Enter the pattern : \n");
+  scanf("%s",pattern);
+  
+  int result = horspool(text,pattern);
+  if(result == -1)
+    printf("\n Pattern is not found in the given text\n");
+  else
+    printf("\n Pattern is found at the index %d in the given text\n",result);
+  
+  return 0;
 }
