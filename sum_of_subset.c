2494f140a0b9251894645281ea05d34669be9a8a
Manoj Kumar BV <135972453+Manoj-Kumar-BV@users.noreply.github.com>
2024-10-16 00:38:04 +0545

    Created easy sum_of_subset.c

---
+++ sum_of_subset.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+
+int subset[MAX], x[MAX], target, solution = 1;
+void sum_subset(int s, int k, int total){
+  x[k] = 1;
+  if(s+subset[k] == target){
+    printf("\n SUBSET %d\n",solution++);
+    for(int i=1;i<=k;i++){
+      if(x[i] == 1)
+        printf("%d\t",subset[i]);
+    }
+    printf("\n");
+  }
+  else if(s+subset[k]+subsetk+1] <= target)
+    sum_subset(s+subset[k],k+1,total-subset[k]);
+  if((s+total-subset[k] >= match) && (s+subset[k+1] <= match)){
+    x[k] == 0;
+    sum_subset(s,k+1,total-subset[k]);
+  }
+}
+
+int main(){
+  int i,n,sum=0;
+  printf("\n Enter the no. of values: \n");
+  scanf("%d",&n);
+  printf("\n Enter the %d values in ascending order : \n");
+  for(i=1;i<=n;i++){
+    scanf("%d",subset[i]);
+    sum += subset[i];
+  }
+  printf("\n Enter the target value : \n");
+  scanf("%d",&target);
+
+  if(sum<match||subset[1]>match)
+    printf("\n No Solution\n");
+  sum_subset(0,1,sum);
+  return 0;
+}
